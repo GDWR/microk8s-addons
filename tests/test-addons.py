7c18af01f8226d17caf677301801d0b2f7bff730
GDWR <gregory.dwr@gmail.com>
2022-07-06 22:26:46 +0100

    Add addon grafana

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -18,3 +18,11 @@ class TestAddons(object):
         status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
         expected = {"colours": "enabled"}
         microk8s_disable("colours")
+
+    def test_grafana(self):
+        microk8s_enable("grafana")
+        wait_for_pod_state("", "default", "running", label="app=grafana")
+        status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
+        print(status)
+        expected = {"grafana": "enabled"}
+        microk8s_disable("grafana")
